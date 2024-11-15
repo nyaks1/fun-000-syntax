f8569538216539ae9a84c0672203f72a49a3da6c
fempotujhb024 <fempotujhb024@student.wethinkcode.co.za>
2024-11-15 11:42:24 +0200

    adjusted the testing

--- tests/test_my_tests.py
+++ tests/test_my_tests.py
@@ -1,24 +1,21 @@
 import unittest
 from fundamentals import check_number
-    
+
 class TestNumber(unittest.TestCase):
     def test_check_number_odd_number(self):
-        self.assertEqual(check_number(1,19))
+        self.assertEqual(check_number(1,19), (1,3,5,7,9))
 
     def test_check_number_even_range_2_to_5(self):
-        self.assertEqual(check_number(2, 5))
+        self.assertEqual(check_number(2, 5), (4))
 
     def test_check_number_even_range_6_to_20(self):
-        self.assertEqual(check_number(6, 20))
-
-    def test_check_number_even_range_6_to_20(self):
-        self.assertEqual(check_number(6, 20))
+        self.assertEqual(check_number(6, 20), (8,10,12,14,16,18))
 
     def test_check_number_negative_even_number(self):
-        self.assertEqual(check_number(-2, -20))
+        self.assertEqual(check_number(-2, -20), (-2,-4,-6,-8,10,-12,-14,-16,-18,-20))
 
     def test_check_number_negative_odd_number(self):
-        self.assertEqual(check_number(-1, -19))
+        self.assertEqual(check_number(-1, -19), (-1,-3,-5,-7,9,-11,-13,-15,-17,-19))
 
     def test_check_number_neutral(self):
         self.assertEqual(check_number(0))
